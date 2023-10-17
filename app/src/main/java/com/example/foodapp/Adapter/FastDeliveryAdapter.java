66fe667dc3fbe71f86d70c7c848cbc280ccf6b6b
Georgin Joseph <georginjoseph3521@gmail.com>
2023-10-17 23:54:34 +0530

    image is retrive from the firestore

--- app/src/main/java/com/example/foodapp/Adapter/FastDeliveryAdapter.java
+++ app/src/main/java/com/example/foodapp/Adapter/FastDeliveryAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.bumptech.glide.Glide;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,22 +44,27 @@ public class FastDeliveryAdapter extends RecyclerView.Adapter<FastDeliveryAdapte
         holder.price.setText(fastDeliveryDomain.getItemPrice());
 
         String documentId = fastDeliveryDomain.getItemId();
+        String imageUrl = fastDeliveryDomain.getImageUrl();
+
+        // Load and display the image using Glide
+        Glide.with(context)
+                .load(imageUrl)
+                .placeholder(R.drawable.fast_1) // Optional placeholder image
+                .into(holder.pic);
+
         // Set an OnClickListener for the card view
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Launch the item_details activity
-
                 Intent intent = new Intent(context, item_details.class);
                 intent.putExtra("item_id", documentId);
                 context.startActivity(intent);
-
                 Toast.makeText(context, "Item ID: " + documentId, Toast.LENGTH_SHORT).show();
-
             }
-
         });
     }
+
 
     @Override
     public int getItemCount() {
@@ -67,12 +73,14 @@ public class FastDeliveryAdapter extends RecyclerView.Adapter<FastDeliveryAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView title, des, price;
+        ImageView pic;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.title);
             des = itemView.findViewById(R.id.des);
             price = itemView.findViewById(R.id.price);
+            pic=itemView.findViewById(R.id.pic);
         }
     }
 }
