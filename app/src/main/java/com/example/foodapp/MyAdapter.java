743edc21211a3825e16304f7fef30ddc539aa1fa
Georgin Joseph <georginjoseph3521@gmail.com>
2023-10-19 23:27:11 +0530

    update_popup

--- app/src/main/java/com/example/foodapp/MyAdapter.java
+++ app/src/main/java/com/example/foodapp/MyAdapter.java
@@ -1,20 +1,29 @@
 package com.example.foodapp;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog; // Add AlertDialog import
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
 
+import com.bumptech.glide.Glide;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -39,13 +48,22 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.ItemName.setText(item.getItemName());
         holder.ItemId.setText(item.getItemId());
 
+        // Set a click listener for the "Edit" button
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve the item's ID for editing
+                String itemId = item.getItemId();
+                showEditDialog(itemId, item);
+            }
+        });
+
         // Set a click listener for the "Delete" button
         holder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Retrieve the item's ID
+                // Retrieve the item's ID for deletion
                 String itemId = item.getItemId();
-                // Show a confirmation dialog
                 showConfirmationDialog(itemId, position);
             }
         });
@@ -58,52 +76,134 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView ItemName, ItemId;
-        Button deleteButton;
+        Button editButton, deleteButton;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             ItemName = itemView.findViewById(R.id.ItemName);
             ItemId = itemView.findViewById(R.id.ItemId);
+            editButton = itemView.findViewById(R.id.edit);
             deleteButton = itemView.findViewById(R.id.delete);
         }
     }
 
-    // Implement the method to delete the item from Firestore using the itemId
-    private void deleteItemFromFirestore(String itemId, int position) {
+    private void showEditDialog(String itemId, manageitems item) {
+        // Create a DialogPlus instance
+        DialogPlus dialog = DialogPlus.newDialog(context)
+                .setGravity(Gravity.CENTER)
+                .setContentHolder(new ViewHolder(R.layout.update_popup))
+                .setExpanded(false)
+                .setMargin(100, 100, 100, 100)
+                .create();
+
+        // Get the root view of the custom layout
+        View rootView = dialog.getHolderView();
+
+        // Retrieve the Spinner view
+        Spinner spinnerCategory = rootView.findViewById(R.id.spinnerCategory);
+
+        // Call the method to retrieve categories and populate the Spinner
+        retrieveCategoriesForSpinner(spinnerCategory, item);
+
+        // Retrieve other views from the layout and set data
+        EditText itemIdEditText = rootView.findViewById(R.id.itemId);
+        EditText itemNameEditText = rootView.findViewById(R.id.itemName);
+        EditText itemPriceEditText = rootView.findViewById(R.id.itemPrice);
+        EditText itemQuantityEditText = rootView.findViewById(R.id.ItemQuantity);
+        EditText itemDescriptionEditText = rootView.findViewById(R.id.ItemDescription);
+
+        // Set the existing item details in the EditText fields
+        itemIdEditText.setText(item.getItemId());
+        itemNameEditText.setText(item.getItemName());
+        itemPriceEditText.setText(String.valueOf(item.getItemPrice()));
+        itemQuantityEditText.setText(item.getItemQuantity());
+        itemDescriptionEditText.setText(item.getItemDescription());
+
+
+        ImageView itemImageView = rootView.findViewById(R.id.imageView);
+        Glide.with(context)
+                .load(item.getImageUrl())
+                .into(itemImageView);
+
+        // Show the DialogPlus dialog
+        dialog.show();
+        // Inside the showEditDialog method
+// Inside the showEditDialog method
+
+
+
+//        Button updateButton = rootView.findViewById(R.id.btnupdate);
+//
+//        updateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Retrieve new data from the EditText fields and Spinner
+//                String newItemId = itemIdEditText.getText().toString();
+//                String newItemName = itemNameEditText.getText().toString();
+//                String newItemPrice = itemPriceEditText.getText().toString(); // Changed to String
+//                String newItemCategory = spinnerCategory.getSelectedItem().toString();
+//                String newItemQuantity = itemQuantityEditText.getText().toString();
+//                String newItemDescription = itemDescriptionEditText.getText().toString();
+//
+//                // Create a new manageitems object with the updated data
+//                manageitems updatedItem = new manageitems(newItemId, newItemName, newItemPrice, newItemCategory, newItemQuantity, newItemDescription);
+//
+//                // Update the Firestore document with the new data
+//                FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                db.collection("items")
+//                        .document(item.getItemId()) // Use the original document ID
+//                        .set(updatedItem) // Use .set() or .update() to update the document
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                showToast("Item updated successfully!");
+//                                dialog.dismiss(); // Close the dialog after the update
+//                            }
+//                        });
+//
+//            }
+//        });
+
+
+
+
+
+
+    }
+    private void retrieveCategoriesForSpinner(Spinner spinnerCategory, manageitems item) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // Specify the collection and document to delete using the provided itemId
         db.collection("items")
-                .whereEqualTo("itemId", itemId)
+                .orderBy("category") // Use "category" if that's the field name in your Firestore documents
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
+                    ArrayList<String> categories = new ArrayList<>();
                     for (int i = 0; i < queryDocumentSnapshots.size(); i++) {
-                        String docId = queryDocumentSnapshots.getDocuments().get(i).getId();
-                        db.collection("items").document(docId)
-                                .delete()
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        showToast("Item deleted successfully!");
-                                        // Remove the item from the list and refresh the RecyclerView
-                                        list.remove(position);
-                                        notifyDataSetChanged();
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        showToast("Failed to delete item. Error: " + e.getMessage());
-                                    }
-                                });
+                        String category = queryDocumentSnapshots.getDocuments().get(i).getString("category"); // Use "category"
+                        if (category != null && !categories.contains(category)) {
+                            categories.add(category);
+                        }
                     }
+
+                    // Create an ArrayAdapter for the Spinner and set the data
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, categories);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinnerCategory.setAdapter(adapter);
+
+                    // Set the existing item category using the Spinner
+                    spinnerCategory.setSelection(categories.indexOf(item.getItemCategory()));
                 })
                 .addOnFailureListener(e -> {
-                    showToast("Error querying Firestore: " + e.getMessage());
+                    showToast("Error querying Firestore for categories: " + e.getMessage());
                 });
+
+
+
     }
 
-    // Method to show a confirmation dialog
+
+
+
     private void showConfirmationDialog(String itemId, int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Confirm Deletion");
@@ -119,6 +219,38 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         });
 
         builder.show();
+    }
+
+    private void deleteItemFromFirestore(String itemId, int position) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("items")
+                .whereEqualTo("itemId", itemId)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (int i = 0; i < queryDocumentSnapshots.size(); i++) {
+                        String docId = queryDocumentSnapshots.getDocuments().get(i).getId();
+                        db.collection("items").document(docId)
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        showToast("Item deleted successfully!");
+                                        list.remove(position);
+                                        notifyDataSetChanged();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        showToast("Failed to delete item. Error: " + e.getMessage());
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    showToast("Error querying Firestore: " + e.getMessage());
+                });
     }
 
     private void showToast(String message) {
--- app/src/main/java/com/example/foodapp/manageitems.java
+++ app/src/main/java/com/example/foodapp/manageitems.java
@@ -2,7 +2,47 @@ package com.example.foodapp;
 
 public class manageitems{
 
-   private String itemName,itemId,itemQuantity;
+   private String itemName;
+    private String itemId;
+    private String itemQuantity;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    private String imageUrl;
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    private String itemPrice;
+    private String itemCategory;
+    private String itemDescription;
 
     private String documentId;
 
@@ -14,6 +54,8 @@ public class manageitems{
     public void setDocumentId(String documentId) {
         this.documentId = documentId;
     }
+
+
     public String getItemName() {
         return itemName;
     }
